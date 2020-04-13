295d023e94fa52add2246d8350b6c20f35f72023
danil <gre4kaDISCOUNT@gmail.com>
2020-04-13 12:39:10 +0900

    render news on page load

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// Custom Http Module
+//* Custom Http Module
 function customHttp() {
   return {
     get(url, cb) {
       try {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.addEventListener('load', () => {
+        xhr.open("GET", url);
+        xhr.addEventListener("load", () => {
           if (Math.floor(xhr.status / 100) !== 2) {
             cb(`Error. Status code: ${xhr.status}`, xhr);
             return;
@@ -14,7 +14,7 @@ function customHttp() {
           cb(null, response);
         });
 
-        xhr.addEventListener('error', () => {
+        xhr.addEventListener("error", () => {
           cb(`Error. Status code: ${xhr.status}`, xhr);
         });
 
@@ -26,8 +26,8 @@ function customHttp() {
     post(url, body, headers, cb) {
       try {
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.addEventListener('load', () => {
+        xhr.open("POST", url);
+        xhr.addEventListener("load", () => {
           if (Math.floor(xhr.status / 100) !== 2) {
             cb(`Error. Status code: ${xhr.status}`, xhr);
             return;
@@ -36,7 +36,7 @@ function customHttp() {
           cb(null, response);
         });
 
-        xhr.addEventListener('error', () => {
+        xhr.addEventListener("error", () => {
           cb(`Error. Status code: ${xhr.status}`, xhr);
         });
 
@@ -53,11 +53,71 @@ function customHttp() {
     },
   };
 }
-// Init http module
+//* Init http module
 const http = customHttp();
-console.log(http)
 
-//  init selects
-document.addEventListener('DOMContentLoaded', function() {
+const newsServise = (function () {
+  const apiKey = "b74aa85de1074e34b0f1e27a5f6719cb";
+  const apiUrl = "https://newsapi.org/v2";
+
+  return {
+    topHeadlines(country = "ru", cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
+//*  init selects
+document.addEventListener("DOMContentLoaded", function () {
   M.AutoInit();
+  loadNews();
 });
+
+//* load news function
+function loadNews() {
+  newsServise.topHeadlines("ru", onGetResponse);
+}
+
+//* function on get response from server
+function onGetResponse(err, res) {
+  renderNews(res.articles);
+}
+
+//* function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector(".news-container .row");
+  let fragment = "";
+
+  news.forEach((newsItem) => {
+    const el = tamplateNewsItem(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML("afterbegin", fragment);
+}
+
+//* function tamplate news item
+function tamplateNewsItem({ description, title, url, urlToImage }) {
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage || ""}">
+          <span class="card-title">${title || ""}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ""}</p>
+        </div>
+        <div class ="card-action">
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+`;
+}
